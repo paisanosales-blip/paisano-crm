57f395ab8d1a08aae6e681424e999e417b979b66
Paisano Sales <paisanosales@gmail.com>
2026-01-30 13:03:01 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9697733496-6a367",
+  "appId": "1:965404104907:web:d61a77cedd9fdccf1ee34e",
+  "apiKey": "AIzaSyDrz-U2R2ujQvF3W0_g58TCbPM4QbH4Upo",
+  "authDomain": "studio-9697733496-6a367.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "965404104907"
+};
