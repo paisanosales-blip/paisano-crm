159d7a174e6fadd06ecff5e4aac94396ba3f3ddc
Paisano Sales <paisanosales@gmail.com>
2026-01-30 14:40:08 +0000

    CONFIGURA MI STORE BUCKET gs://studio-9697733496-6a367.firebasestorage.a

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:965404104907:web:d61a77cedd9fdccf1ee34e",
   "apiKey": "AIzaSyDrz-U2R2ujQvF3W0_g58TCbPM4QbH4Upo",
   "authDomain": "studio-9697733496-6a367.firebaseapp.com",
+  "storageBucket": "studio-9697733496-6a367.appspot.com",
   "measurementId": "",
   "messagingSenderId": "965404104907"
 };
