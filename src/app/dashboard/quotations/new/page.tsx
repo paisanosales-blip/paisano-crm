cf5088783cad5dc83b4cfa9fbe747048717b34fc
Paisano Sales <paisanosales@gmail.com>
2026-02-03 20:30:22 +0000

    eL TAMAÑO DEL LOGO EN 65 UNIDADES

--- src/app/dashboard/quotations/new/page.tsx
+++ src/app/dashboard/quotations/new/page.tsx
@@ -222,7 +222,7 @@ export default function NewQuotationPage() {
           const format = logoUrl.substring(logoUrl.indexOf('/') + 1, logoUrl.indexOf(';'));
           const img = new Image();
           img.src = logoUrl;
-          const imgWidth = 35;
+          const imgWidth = 65;
           docPdf.addImage(logoUrl, format.toUpperCase(), margin, 3, imgWidth, 0, undefined, 'NONE');
         } catch (e) {
           console.error("Error adding logo image to PDF:", e);
