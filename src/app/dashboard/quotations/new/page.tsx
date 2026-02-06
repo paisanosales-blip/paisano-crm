bc4f2d62cf5464aa6913684362ae34deec9517cd
Paisano Sales <paisanosales@gmail.com>
2026-02-06 20:57:10 +0000

    eL LOGO que quede debajo de la linea roja. La linea al frente.

--- src/app/dashboard/quotations/new/page.tsx
+++ src/app/dashboard/quotations/new/page.tsx
@@ -241,7 +241,7 @@ export default function NewQuotationPage() {
           });
           const format = blob.type.split('/')[1];
           const imgWidth = 65;
-          docPdf.addImage(img.src, format.toUpperCase(), margin, 0, imgWidth, 0, undefined, 'NONE');
+          docPdf.addImage(img.src, format.toUpperCase(), margin, 5, imgWidth, 0, undefined, 'NONE');
         } catch (e) {
             console.error("Error adding logo image to PDF:", e);
         }
