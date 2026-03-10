c384e96b42bf8483f35d9fc7cdf811785b6e6a6a
Paisano Sales <paisanosales@gmail.com>
2026-03-10 13:36:52 +0000

    Try fixing this error: `Recoverable Error: Hydration failed because the

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -13,10 +13,15 @@ import * as React from "react"
 
 export function Toaster() {
   const { toasts } = useToast()
+  const [isMounted, setIsMounted] = React.useState(false)
+
+  React.useEffect(() => {
+    setIsMounted(true)
+  }, [])
 
   return (
     <ToastProvider duration={3000}>
-      {toasts.map(function ({ id, title, description, action, ...props }) {
+      {isMounted && toasts.map(function ({ id, title, description, action, ...props }) {
         return (
           <Toast key={id} {...props} className="pointer-events-auto">
             <div className="grid gap-1">
